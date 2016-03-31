eec4c26bc9edbc1354d93e8f033cc9973861cff8
sathishnec <sathish_nec@yahoo.co.in>
2016-03-31 21:44:49 +0100

    Insert posts into the database and also included the javascript validation using the JS library parsley.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -36,7 +37,23 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the data
+        $this->validate($request, array(
+                'title' =>'required|max:255',
+                'body' => 'required'
+            ));
+
+        // Store in the database
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        $post->save();
+
+        // Redirect the user 
+        return redirect()->route('posts.show',$post->id);
+
     }
 
     /**
